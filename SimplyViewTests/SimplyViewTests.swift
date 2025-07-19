e9cac0af9e4ea0d08a4c28aeb1404f9ef4aeb706
cassaicus <cassaicus@gmail.com>
2025-07-19 19:23:49 +0900

    Initial Commit

---
+++ SimplyViewTests/SimplyViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimplyViewTests.swift
+//  SimplyViewTests
+//
+//  Created by ibis on 2025/07/19.
+//
+
+import Testing
+
+struct SimplyViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
