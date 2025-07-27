135c74f3d2f3fc700c27722a68d34b14c8126f76
cassaicus <cassaicus+g@gmail.com>
2025-07-27 14:43:48 +0900

    Initial Commit

---
+++ SimplyView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimplyView
+//
+//  Created by ibis on 2025/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimplyView/SimplyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyViewApp.swift
+//  SimplyView
+//
+//  Created by ibis on 2025/07/27.
+//
+
+import SwiftUI
+
+@main
+struct SimplyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
